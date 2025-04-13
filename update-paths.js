895694c535ad11c107efd19a5c55a457d5f3ee8a
oebelus <imaneelfidrissi@hotmail.com>
2025-04-14 00:37:09 +0100

    workflow

--- update-paths.js
+++ update-paths.js
@@ -1,16 +1,53 @@
 import fs from "fs";
 import path from "path";
 
-function updateFilePaths(filePath) {
-  let content = fs.readFileSync(filePath, "utf8");
+function updateScriptPaths(directory) {
+  const files = fs.readdirSync(directory);
 
-  content = content.replace(
-    /(href|src)=["'](?!\/fractals-mania\/)(\/[^"']*)["']/g,
-    (match, attr, urlPath) => `${attr}="/fractals-mania${urlPath}"`
-  );
+  files.forEach((file) => {
+    const filePath = path.join(directory, file);
+    const stats = fs.statSync(filePath);
 
-  fs.writeFileSync(filePath, content);
+    if (stats.isDirectory()) {
+    } else if (file.endsWith(".html")) {
+      let content = fs.readFileSync(filePath, "utf8");
+
+      content = content.replace(
+        /(href|src)=["']([^"']*)["']/g,
+        (match, attr, urlPath) => {
+          if (
+            urlPath.startsWith("http") ||
+            urlPath.startsWith("#") ||
+            urlPath.startsWith("data:")
+          ) {
+            return match;
+          }
+
+          const cleanPath = urlPath.replace(/^\/?fractals-mania\//, "");
+
+          // Handle different path cases
+          if (cleanPath.startsWith("/")) {
+            return `${attr}="/fractals-mania${cleanPath}"`;
+          } else if (
+            cleanPath.startsWith("./") ||
+            cleanPath.startsWith("../")
+          ) {
+            const absolutePath = path.resolve(
+              "/",
+              path.dirname(filePath),
+              cleanPath
+            );
+            return `${attr}="/fractals-mania${absolutePath}"`;
+          } else {
+            return `${attr}="/fractals-mania/${cleanPath}"`;
+          }
+        }
+      );
+
+      fs.writeFileSync(filePath, content);
+      console.log(`Updated paths in: ${filePath}`);
+    }
+  });
 }
 
-updateFilePaths("./dist/pages");
-updateFilePaths("./dist");
+updateScriptPaths("./dist");
